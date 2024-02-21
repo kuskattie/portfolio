ba940436a431918d86384c7f20e507875aeb1696
KusKattie <e.lujnova@gmail.com>
2024-02-21 21:05:05 +0100

    chaneg in project items

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ProjectItem = ({img, title}) => {
+const ProjectItem = ({ img, title }) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#0e7490]'>
-        <img src={img} alt='/' className='rounded-xl group-hover:opacity-10' />
-        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
-                {title}
-            </h3>
-            <a href='/'>
-                <p className='text-cemter p-3 rounded-ld bg-white text-gray-700 font-bold cursor-pointer text-lg'>More info</p>
-            </a>
-        </div>
+      <img src={img} alt='/' className='rounded-xl group-hover:opacity-10' />
+      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <h3 className='text-2xl font-bold tracking-wider text-center text-white'>
+          {title}
+        </h3>
+        {/* Используем Link вместо <a> для навигации */}
+        <Link to='/'>
+          <p className='p-3 text-lg font-bold text-center text-gray-700 bg-white cursor-pointer rounded-ld'>More info</p>
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default ProjectItem
+export default ProjectItem;
