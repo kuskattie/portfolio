962f6bbee4a2120cedeae28fa8e8e153ed1efddd
KusKattie <e.lujnova@gmail.com>
2024-02-21 20:50:01 +0100

    Добавлен роутинг с помощью React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'; // Импортируем BrowserRouter из React Router
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Оборачиваем корневой компонент в BrowserRouter */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
